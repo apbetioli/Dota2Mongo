640fceb4166e68d9e173962974de0af826376837
Alexandre Betioli <apbetioli@gmail.com>
2015-06-28 23:14:19 -0300

    Fixed bugs

--- matches.js
+++ matches.js
@@ -5,29 +5,7 @@ var util = require('./util');
 
 var counter = 1;
 
-db.matches.find().sort({match_seq_num:1}).limit(1, function(err, docs) {
-    getMatches(docs[0].match_seq_num);
-});
-
-function getMatches(lastMatchSeqNum) {
-    var url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001?key=" + config.get('apikey');
-    if (lastMatchSeqNum)
-        url += "&start_at_match_seq_num=" + lastMatchSeqNum;
-
-    console.log(counter + ' ' + util.time() + ' ' + url);
-
-    util.get(url, function (result) {
-        for (i in result.matches) {
-            var match = result.matches[i];
-            persist(match);
-        }
-
-        counter++;
-        getMatches(lastMatchSeqNum-100);
-    });
-}
-
-var persist = function(result) {
+function insert(result) {
     try {
         if (!result.match_id) {
             console.log('Invalid match: ' + result.match_id + ' - ' + JSON.stringify(result));
@@ -41,9 +19,31 @@ var persist = function(result) {
     }
 }
 
+function getMatches(lastMatchSeqNum) {
+    var url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/V001?key=" + config.get('apikey');
+    if (lastMatchSeqNum)
+        url += "&start_at_match_seq_num=" + lastMatchSeqNum;
+
+    console.log(counter + ' ' + util.time() + ' ' + url);
+
+    util.get(url, function (result) {
+        for (var i in result.matches) {
+            var match = result.matches[i];
+            insert(match);
+        }
+
+        counter++;
+        getMatches(lastMatchSeqNum-100);
+    });
+}
+
+db.matches.find().sort({match_seq_num:1}).limit(1, function(err, docs) {
+    getMatches(docs[0].match_seq_num-100);
+});
+
 /*
 function getMatch(match_id) {
     var url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=" + match_id + "&key=" + config.get('apikey');
-    util.get(match_id, url, persist);
+    util.get(match_id, url, insert);
 }
 */
